3baee8755103c59e0b9daa0a7ec104997a26913e
your new username <smallmonster900@gmail.com>
2024-07-11 20:27:26 +0700

    a

--- src/UpdateQuantity.jsx
+++ src/UpdateQuantity.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { decreaseQuantity, increaseQuantity } from './redux/actions/quantity';
 
 function UpdateQuantity() {
     const quantity = useSelector((state) => {
         return state.quantity;
-    })
+    });
+
+    const dispath = useDispatch();
+    const increaseQty = () => {
+        dispath(increaseQuantity(1));
+    }
+    const decreaseQty = () => {
+        dispath(decreaseQuantity(1))
+    }
     return (
         <>
-            <button>-</button>
+            <button onClick={decreaseQty}>-</button>
             <input type="text" value={quantity} readOnly />
-            <button>+</button>
+            <button onClick={increaseQty}>+</button>
         </>
     )
 }
